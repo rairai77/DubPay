a92e7097c4560fe83bafb35d30711472d1e8e598
rairai77 <cutenitki@gmail.com>
2025-02-26 22:52:31 -0800

    added debug messages to build and made sure tokio features were on

--- build.rs
+++ build.rs
@@ -1,12 +1,9 @@
 fn main() {
+    println!("Compiling Protobuf files");
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile_protos(
-            &[
-                "proto/transaction.proto"
-            ],
-            &["proto"],
-        )
+        .compile_protos(&["proto/transaction.proto"], &["proto"])
         .expect("Failed to compile Protobuf files");
+    println!("Compiled Protobuf files");
 }
