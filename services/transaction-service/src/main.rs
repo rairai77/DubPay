f720dbf1f68b7eb9d0ae67089a393d1c900e331b
rairai77 <cutenitki@gmail.com>
2025-03-10 20:47:06 -0700

    added a single communication

--- services/transaction-service/src/main.rs
+++ services/transaction-service/src/main.rs
@@ -1,13 +1,14 @@
 use dubpay::transaction_handler_server::{TransactionHandler, TransactionHandlerServer};
 use dubpay::{
-    ConfirmRequestRequest, ConfirmRequestResponse, RequestMoneyRequest, RequestMoneyResponse,
-    SendPaymentRequest, SendPaymentResponse,
+    notification_handler_client, ConfirmRequestRequest, ConfirmRequestResponse,
+    RequestMoneyRequest, RequestMoneyResponse, SendPaymentRequest, SendPaymentResponse,
 };
 use std::env;
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod dubpay {
     tonic::include_proto!("dubpay.transaction");
+    tonic::include_proto!("dubpay.notification");
 }
 
 #[derive(Debug, Default)]
@@ -31,9 +32,47 @@ impl TransactionHandler for TransactionService {
 
     async fn send_payment(
         &self,
-        _request: Request<SendPaymentRequest>,
+        request: Request<SendPaymentRequest>,
     ) -> Result<Response<SendPaymentResponse>, Status> {
-        Err(Status::unimplemented("Not yet implemented"))
+        let request_content = request.into_inner();
+        let from = request_content.from_user_id;
+        let to = request_content.to_user_id;
+        let amount = request_content.amount;
+        let public = request_content.public;
+        let description = request_content.description;
+
+        println!(
+            "Received payment request from {} to {} for {}, public: {}, description: {}",
+            from, to, amount, public, description
+        );
+
+        let mut notification_handler_client =
+            notification_handler_client::NotificationHandlerClient::connect(
+                "http://notification-service:50051",
+            )
+            .await
+            .unwrap();
+
+        let notification_request = tonic::Request::new(dubpay::SendNotificationRequest {
+            user_id: to,
+            message: format!("You received a payment of {} from {}", amount, from),
+            timestamp: Some(prost_types::Timestamp {
+                seconds: 0,
+                nanos: 0,
+            }),
+        });
+
+        let response = notification_handler_client
+            .send_notification(notification_request)
+            .await
+            .unwrap();
+
+        println!("Notification response: {:?}", response.into_inner().message);
+
+        return Ok(Response::new(SendPaymentResponse {
+            id: "1".to_string(),
+            message: "success".to_string(),
+        }));
     }
 }
 
